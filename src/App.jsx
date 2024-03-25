57fd40fd59ec426ffdf5733fd2bdd8e956ef57ab
MichalSliwaSE <112940119+MichalSliwaSE@users.noreply.github.com>
2024-03-25 23:46:07 +0100

    feat: add componenets, styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
-import reactImg from "./assets/react-core-concepts.png";
+
 import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx"
+import CoreConcept from "./components/CoreConcept.jsx";
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[getRandomInt(reactDescriptions.length)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
     <div>
       <Header />
-
       <main>
         <section id="core-concepts">
           <ul>
