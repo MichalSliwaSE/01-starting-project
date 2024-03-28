51e28cb66b22d5379b8c35ccb238d868825f5aef
MichalSliwaSE <112940119+MichalSliwaSE@users.noreply.github.com>
2024-03-28 09:35:53 +0100

    feat: add tab button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-
 import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/Header/Header.jsx"
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
-
-
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -12,17 +10,25 @@ function App() {
       <main>
         <section id="core-concepts">
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
-             <CoreConcept
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept
               title={CORE_CONCEPTS[1].title}
-              image ={CORE_CONCEPTS[1].image}
+              image={CORE_CONCEPTS[1].image}
               description={CORE_CONCEPTS[1].description}
             />
-             <CoreConcept {...CORE_CONCEPTS[2]}/>
-             <CoreConcept {...CORE_CONCEPTS[3]}/>
-           
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
           <h2>Time to get started!</h2>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Text</TabButton>
+            <TabButton>Text</TabButton>
+            <TabButton>Text</TabButton>
+            <TabButton>Text</TabButton>
+          </menu>
         </section>
       </main>
     </div>
